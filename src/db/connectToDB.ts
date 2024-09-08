01287074986dcad3fdfad9fd3d62ed654684bae2
saifullah-10 <saifullah.bus@gmail.com>
2024-09-08 20:55:39 +0600

    update database connection

--- src/db/connectToDB.ts
+++ src/db/connectToDB.ts
@@ -1,8 +1,8 @@
 import { MongoClient, ServerApiVersion } from "mongodb";
 import dotEnv from "dotenv";
 dotEnv.config();
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.kpsyb7k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.kpsyb7k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.wxzps.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 let client: any;
 
 export const connectToDatabase = async () => {
