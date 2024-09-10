7f9900d9be24907b175b3ee24dbb82bdcd7f1fdd
saifullah-10 <saifullah.bus@gmail.com>
2024-09-10 18:28:44 +0600

    change method

--- dist/router/authRoute.js
+++ dist/router/authRoute.js
@@ -5,7 +5,7 @@ const authMiddlewares_1 = require("../middlewares/authMiddlewares");
 exports.default = (router) => {
     router.post("/auth/register", authentication_1.registration);
     router.post("/auth/login", authentication_1.login);
-    router.post("/auth/logout", authMiddlewares_1.isAuthenticate, authentication_1.logoutUser);
+    router.get("/auth/logout", authMiddlewares_1.isAuthenticate, authentication_1.logoutUser);
     router.get("/auth/protected", authMiddlewares_1.isAuthenticate, authentication_1.isUser);
 };
 //# sourceMappingURL=authRoute.js.map
--- src/router/authRoute.ts
+++ src/router/authRoute.ts
@@ -10,6 +10,6 @@ import { isAuthenticate, isOwner } from "../middlewares/authMiddlewares";
 export default (router: express.Router) => {
   router.post("/auth/register", registration);
   router.post("/auth/login", login);
-  router.post("/auth/logout", isAuthenticate, logoutUser);
+  router.get("/auth/logout", isAuthenticate, logoutUser);
   router.get("/auth/protected", isAuthenticate, isUser);
 };
