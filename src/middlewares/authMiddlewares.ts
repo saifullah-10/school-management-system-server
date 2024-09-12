f749b365e13def0f8470aace29dff4ed7a14b986
saifullah-10 <saifullah.bus@gmail.com>
2024-09-12 09:37:16 +0600

    update middleware

--- src/middlewares/authMiddlewares.ts
+++ src/middlewares/authMiddlewares.ts
@@ -2,7 +2,8 @@ import express from "express";
 import { getUserByEmail } from "../db/user";
 import jwt from "jsonwebtoken";
 import dotEnv from "dotenv";
-import { merge } from "lodash";
+import { get, merge } from "lodash";
+import { User } from "authTypes";
 dotEnv.config();
 export const isAuthenticate = async (
   req: express.Request,
@@ -11,10 +12,14 @@ export const isAuthenticate = async (
 ) => {
   res.header(
     "Access-Control-Allow-Origin",
+
     "https://starlight-un-edu.vercel.app"
   );
   res.header("Access-Control-Allow-Credentials", "true");
   const authHeader = req.headers["authorization"];
+  // const authHeader =
+  //   "Beareer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRha2lAZ21haWwuY29tIiwiaWF0IjoxNzI2MTEwMDE3LCJleHAiOjE3MjYxMTM2MTd9.suTCIXXvYIFok0ijIAfaM-rAUaP_R1YHKzu9NcR8Y48";
+
   if (!authHeader) {
     return res.status(403).json({ message: "Forbidden" });
   }
@@ -25,12 +30,74 @@ export const isAuthenticate = async (
   try {
     const decoded: any = jwt.verify(token, process.env.JWT_SECRET);
     const user = await getUserByEmail(decoded.email);
-    merge(req, { identity: user });
-    next();
+    if (user) {
+      merge(req, { identity: user });
+      next();
+    } else {
+      return res.status(403).json({ message: "Forbidden" });
+    }
   } catch (err) {
     return res.status(403).json({ message: "Forbidden" });
   }
 };
+
+export const isAdmin = async (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) => {
+  const user: User = get(req, "identity");
+
+  if (user) {
+    if (user.role === "admin") {
+      next();
+    } else {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+  } else {
+    return res.status(403).json({ message: "Forbidden" });
+  }
+};
+export const isStudent = async (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) => {
+  const user: User = get(req, "identity");
+
+  if (user) {
+    if (user.role === "student") {
+      next();
+    } else {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+  } else {
+    return res.status(403).json({ message: "Forbidden" });
+  }
+};
+export const isTeacher = async (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) => {
+  const user: User = get(req, "identity");
+
+  if (user) {
+    if (user.role === "teacher") {
+      next();
+    } else {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+  } else {
+    return res.status(403).json({ message: "Forbidden" });
+  }
+};
+
+// export const isStudent = async (
+//   req: express.Request,
+//   res: express.Response,
+//   next: express.NextFunction
+// ) => {};
 
 // export const isOwner = async (
 //   req: express.Request,
