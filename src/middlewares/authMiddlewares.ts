6725ab2cea0a857175232d26872d99718c36e2e8
saifullah-10 <saifullah.bus@gmail.com>
2024-09-12 07:39:52 +0600

    update middleware

--- src/middlewares/authMiddlewares.ts
+++ src/middlewares/authMiddlewares.ts
@@ -1,31 +1,31 @@
-// import express from "express";
-// import { getUserByEmail } from "../db/user";
-// import jwt from "jsonwebtoken";
-// import dotEnv from "dotenv";
-// import { get, merge } from "lodash";
-// dotEnv.config();
-// export const isAuthenticate = async (
-//   req: express.Request,
-//   res: express.Response,
-//   next: express.NextFunction
-// ) => {
-//   const authHeader = req.headers["authorization"];
-//   if (!authHeader) {
-//     return res.status(403).json({ message: "Forbidden" });
-//   }
-//   const token = authHeader.split(" ")[1];
-//   if (token === null) {
-//     return res.status(401);
-//   }
-//   try {
-//     const decoded: any = jwt.verify(token, process.env.JWT_SECRET);
-//     const user = await getUserByEmail(decoded.email);
-//     merge(req, { identity: user });
-//     next();
-//   } catch (err) {
-//     return res.status(403).json({ message: "Forbidden" });
-//   }
-// };
+import express from "express";
+import { getUserByEmail } from "../db/user";
+import jwt from "jsonwebtoken";
+import dotEnv from "dotenv";
+import { merge } from "lodash";
+dotEnv.config();
+export const isAuthenticate = async (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) => {
+  const authHeader = req.headers["authorization"];
+  if (!authHeader) {
+    return res.status(403).json({ message: "Forbidden" });
+  }
+  const token = authHeader.split(" ")[1];
+  if (token === null) {
+    return res.status(401);
+  }
+  try {
+    const decoded: any = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await getUserByEmail(decoded.email);
+    merge(req, { identity: user });
+    next();
+  } catch (err) {
+    return res.status(403).json({ message: "Forbidden" });
+  }
+};
 
 // export const isOwner = async (
 //   req: express.Request,
